3261972c4d19ef4715ca0ba84d247e44dbf106fc
Mikhail Pelagein <retvain@gmail.com>
2022-05-15 21:15:21 +0500

    scan folders and set in redis

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"context"
 	"fmt"
-
-	//"fmt"
+	"github.com/go-redis/redis/v8"
 	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
 )
+
+type redisCache struct {
+	host    string
+	db      int
+	expires time.Duration
+}
+
+var ctx = context.Background()
 
 func main() {
 	files, err := ioutil.ReadDir("./")
@@ -14,7 +26,43 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for _, f := range files {
-		fmt.Println(f.Name())
+	filesList := make([]string, 0, len(files))
+
+	path, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	key := 0
+	for _, file := range files {
+		fileName := file.Name()
+		if file.IsDir() {
+			fullPath := filepath.Join(path, fileName)
+			fmt.Println(fullPath)
+			//fmt.Println(fileName, file.IsDir())
+			filesList = append(filesList, fullPath)
+			set(key, fullPath)
+			fmt.Println(key)
+			key++
+		}
+	}
+}
+
+func set(key int, value string) {
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "redis",
+		DB:       0,
+	})
+
+	err := client.Set(
+		ctx,
+		strconv.Itoa(key),
+		value,
+		0).Err()
+
+	if err != nil {
+		log.Fatal(err)
 	}
 }
